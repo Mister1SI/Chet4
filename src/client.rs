da50f06ac9a5d8f1aca112b53716931435e13656
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2023-09-18 17:58:37 +0000

    Start work on client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,19 @@
-pub fn start(addr: &String, uname: &String) {
+use std::net::TcpStream;
+use std::io::{Read, Write};
+use std::io;
+
+pub fn start(addr: &String, uname: &String) -> std::io::Result<()> {
 	println!("{addr}{uname}\nClient!");
+	println!("Type \"exit()\" to quit the program\nEnter messages to send:");
+	let mut stream = TcpStream::connect(addr.as_str())?;
+	loop {
+		let mut msg = String::new();
+		io::stdin().read_line(&mut msg).unwrap_or(0);
+		if msg == "exit()" {
+			break;
+		}
+		stream.write_all(&msg.as_bytes());
+		
+	}
+	Ok(())
 }
