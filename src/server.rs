63c38afd4c47f54d10b904285d689e3b1d425400
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2023-09-18 17:11:41 +0000

    Fix errors in server.rs

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use std::thread;
 
 pub fn start(addr: &String) {
     println!("{}\nServer!", addr);
-	let listener = TcpListener::bind(addr)
+	let listener = TcpListener::bind(addr.as_str())
 		.expect("Error creating the listening server.");
 	
 	for stream in listener.incoming() {
@@ -28,9 +28,9 @@ pub fn start(addr: &String) {
 fn handle_connection(mut s: TcpStream) -> Result<(), std::io::Error> {
 	let mut buffer = [0; 1024];
 	let bytes_read = s.read(&mut buffer)?;
-	let msg = String::from_utf8(buffer[..bytes_read].to_vec())?;
+	let msg = String::from_utf8(buffer[..bytes_read]
+								.to_vec()).unwrap_or(String::from(""));
 	
 	println!("{}", msg);
-
 	Ok(())
 }
